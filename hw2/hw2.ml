14d6cdadb2bace5218c69ab78f914c48e53ce87d
eltonkl <eltonkl@users.noreply.github.com>
2016-04-14 22:29:38 -0700

    Implement evalExp, execute and cleanup problem 1 code for hw2

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -1,5 +1,10 @@
 (* Resources used: MSDN F# documentation
+ * and http://caml.inria.fr/pub/docs/manual-ocaml/libref/List.html
  *
+ * People I talked to:
+     * Shalini Dangi
+     * Neda Vesselinova
+     * James Wang
  *)
 
 
@@ -13,11 +18,11 @@ type matrix = vector list
 
 let (vplus : vector -> vector -> vector) =
     fun v1 v2 ->
-        List.map2 (fun f1 f2 -> f1 +. f2) v1 v2
+        List.map2 (+.) v1 v2
 
 let (mplus : matrix -> matrix -> matrix) =
     fun m1 m2 ->
-        List.map2 (fun v1 v2 -> vplus v1 v2) m1 m2
+        List.map2 vplus m1 m2
 
 let (dotprod : vector -> vector -> float) =
     fun v1 v2 ->
@@ -48,7 +53,8 @@ let (mmult : matrix -> matrix -> matrix) =
         List.mapi
         (
             fun r v1 ->
-                (* dot product the row with the cth column in matrix 2. *)
+                (* collect the dot products of the row with 
+                 * every column in matrix 2 into a vector. *)
                 List.map
                 (fun c -> dotprod v1 c) t2
         ) m1
@@ -59,14 +65,41 @@ let (mmult : matrix -> matrix -> matrix) =
 type op = Plus | Minus | Times | Divide
 type exp = Num of float | BinOp of exp * op * exp
 
-let (evalExp : exp -> float) =
-  raise ImplementMe
+let rec (evalExp : exp -> float) =
+    fun e ->
+        match e with
+        | Num f -> f
+        | BinOp (e1, o, e2) ->
+                let r1 = evalExp e1 in
+                let r2 = evalExp e2 in
+                match o with
+                | Plus -> r1 +. r2
+                | Minus -> r1 -. r2
+                | Times -> r1 *. r2
+                | Divide -> r1 /. r2
 
 (* a type for stack instructions *)	  
 type instr = Push of float | Swap | Calculate of op
 
 let (execute : instr list -> float) =
-  raise ImplementMe
+    fun il ->
+        let rec helper =
+            fun s ilst ->
+                match ilst with
+                | [] -> s
+                | h::t ->
+                        match h with
+                        | Push f -> helper (f::s) t
+                        | Swap ->
+                                let tail = List.tl s in
+                                helper ([List.hd tail; List.hd s] @ List.tl tail) t
+                        | Calculate o ->
+                                let tail = List.tl s in
+                                let r = evalExp
+                                (BinOp (Num (List.hd tail), o, Num (List.hd s))) in
+                                helper (r::(List.tl tail)) t
+        in
+        List.hd (helper [] il)
 
 let (compile : exp -> instr list) =
   raise ImplementMe
