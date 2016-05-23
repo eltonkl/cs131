29054dad94461d58a6490f83b35809fcfc4c2110
eltonkl <eltonkl@users.noreply.github.com>
2016-05-22 20:21:45 -0700

    Add testing code

--- hw5/test.java
+++ hw5/test.java
@@ -3,9 +3,38 @@ import java.io.*;
 class Tester {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         PPMImage img = new PPMImage("florence.ppm");
+        
+        long time0 = System.nanoTime();
         PPMImage negated = img.negate();
+        long time1 = System.nanoTime();
+        System.out.println("Negation:  " + (time1 - time0)/1000000000.);
         negated.toFile("florenceneg.ppm");
+
+        negated.negate().toFile("florencenegneg.ppm");
+
+        time0 = System.nanoTime();
         PPMImage greyscale = img.greyscale();
+        time1 = System.nanoTime();
+        System.out.println("Greyscale: " + (time1 - time0)/1000000000.);
         greyscale.toFile("florencegs.ppm");
+
+        time0 = System.nanoTime();
+        PPMImage mirror = img.mirrorImage();
+        time1 = System.nanoTime();
+        System.out.println("Mirror:    " + (time1 - time0)/1000000000.);
+        mirror.toFile("florencemi.ppm");
+        mirror.mirrorImage().toFile("florencemimi.ppm");
+
+        time0 = System.nanoTime();
+        PPMImage mirror2 = img.mirrorImage2();
+        time1 = System.nanoTime();
+        System.out.println("Mirror2:   " + (time1 - time0)/1000000000.);
+        mirror2.toFile("florencemi2.ppm");
+
+        time0 = System.nanoTime();
+        PPMImage gb1 = img.gaussianBlur(30, 0.5);
+        time1 = System.nanoTime();
+        System.out.println("GB1:       " + (time1 - time0)/1000000000.);
+        gb1.toFile("florencegb1.ppm");
     }
 }
